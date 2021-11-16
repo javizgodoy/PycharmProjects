e70934bfb9652620fc664bc3026cb20bc1f8ee7d
javizgodoy <93671234+javizgodoy@users.noreply.github.com>
2021-11-16 19:47:15 +0100

    sum

---
+++ Aprendiendo Python. Ejercicios/2. Aprendiendo .format.py
@@ -0,0 +1,9 @@
+# Me ha gustado esto de .format. Quiero probar un mensaje automatizado de cumpleaños
+#(que nunca usaré), para felicitar a los amigos cuando llegue el momento.
+
+nombre = input("Escribe su nombre: ")
+edad = int(input("Escribe su edad: "))
+
+print("Felicidades, {}! Te han caído {} palos. Es hora de empezar a cuidarse :)".format(nombre, edad))
+
+# :)
